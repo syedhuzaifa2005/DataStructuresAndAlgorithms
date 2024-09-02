20d636de3a99540416c20d90264393b78811d771
syedhuzaifa2005 <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-09-02 10:32:35 +0500

    Add files via upload

---
+++ Labs/03/Q2.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int value;
+    Node* next;
+
+    Node(int val) : value(val), next(nullptr) {}
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() : head(nullptr) {}
+
+    void append(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    void printList() {
+        Node* current = head;
+        while (current) {
+            scout << current->value << " -> ";
+            current = current->next;
+        }
+        cout << "nullptr" << endl;
+    }
+
+    void reverse() {
+        Node* previous = nullptr;
+        Node* current = head;
+        Node* nextNode = nullptr;
+        while (current) {
+            nextNode = current->next; 
+            current->next = previous; 
+            previous = current;       
+            current = nextNode;       
+        }
+        head = previous;
+    }
+
+    ~LinkedList() {
+        Node* current = head;
+        while (current) {
+            Node* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+};
+
+int main() {
+    LinkedList ll;
+
+    ll.append(1);
+    ll.append(2);
+    ll.append(3);
+    ll.append(4);
+
+    cout << "Original List:" << endl;
+    ll.printList();
+
+    ll.reverse();
+
+    cout << "Reversed List:" << endl;
+    ll.printList();
+
+    return 0;
+}
