b3ba09219802359a1483be97d61eb4d79a8d38a7
Syed Huzaifa Ali <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-10-12 20:24:09 +0500

    Update task04.cpp

--- Labs/06/task04.cpp
+++ Labs/06/task04.cpp
@@ -1,64 +1,95 @@
 #include <iostream>
-#include <stack>
 using namespace std;
 
-class QueueUsingStacks {
-private:
-    stack<int> stack1;
-    stack<int> stack2;
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+class Stack {
+    Node* top;
 
 public:
+    Stack() : top(nullptr) {}
+
+    void push(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    int pop() {
+        if (isEmpty()) {
+            cout << "Stack underflow" << endl;
+            return -1;
+        }
+        int value = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        return value;
+    }
+
+    bool isEmpty() {
+        return top == nullptr;
+    }
+
+    int peek() {
+        if (!isEmpty()) {
+            return top->data;
+        }
+        return -1;
+    }
+};
+
+class Queue {
+    Stack stack1, stack2;
+
+public:
+
     void enqueue(int value) {
         stack1.push(value);
     }
 
     int dequeue() {
-
-        if (stack2.empty()) {
-            while (!stack1.empty()) {
-                stack2.push(stack1.top());
-                stack1.pop();
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            cout << "Queue underflow" << endl;
+            return -1;
+        }
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                int value = stack1.pop();
+                stack2.push(value);
             }
         }
-
-        if (stack2.empty()) {
-            throw runtime_error("Queue is empty");
-        }
-
-        int value = stack2.top();
-        stack2.pop();
-        return value;
+        return stack2.pop();
     }
 
     bool isEmpty() {
-        return stack1.empty() && stack2.empty();
-    }
-
-    int size() {
-        return stack1.size() + stack2.size();
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 };
 
 int main() {
-    QueueUsingStacks queue;
+    Queue q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
 
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
+    cout << "Dequeue: " << q.dequeue() << endl;
+    cout << "Dequeue: " << q.dequeue() << endl;
 
-    cout << "Dequeued: " << queue.dequeue() << endl;
-    cout << "Dequeued: " << queue.dequeue() << endl;
+    q.enqueue(50);
+    q.enqueue(60);
 
-    queue.enqueue(4);
-
-    cout << "Dequeued: " << queue.dequeue() << endl;
-    cout << "Dequeued: " << queue.dequeue() << endl;
-
-    if (queue.isEmpty()) {
-        cout << "Queue is empty." << endl;
-    } else {
-        cout << "Queue is not empty." << endl;
-    }
+    cout << "Dequeue: " << q.dequeue() << endl;
+    cout << "Dequeue: " << q.dequeue() << endl;
+    cout << "Dequeue: " << q.dequeue() << endl;
+    cout << "Dequeue: " << q.dequeue() << endl;
 
     return 0;
 }
