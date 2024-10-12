19a559f6a6ff69c900fd8a160f7be50fab87ca48
Syed Huzaifa Ali <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-10-12 20:20:16 +0500

    Update task01.cpp

--- Labs/06/task01.cpp
+++ Labs/06/task01.cpp
@@ -1,15 +1,55 @@
 #include <iostream>
-#include <stack>
-#include <vector>
 using namespace std;
 
-void separateEvenOdd(const stack<int>& inputStack, stack<int>& evenStack, stack<int>& oddStack) {
-    stack<int> tempStack = inputStack;
+class Node {
+public:
+    int data;
+    Node* next;
 
-    while (!tempStack.empty()) {
-        int value = tempStack.top();
-        tempStack.pop();
-        
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+class Stack {
+    Node* top;
+
+public:
+    Stack() : top(nullptr) {}
+
+    void push(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    int pop() {
+        if (isEmpty()) {
+            cout << "Stack underflow" << endl;
+            return -1;
+        }
+        int value = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        return value;
+    }
+
+    bool isEmpty() {
+        return top == nullptr;
+    }
+
+    void display() {
+        Node* temp = top;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+void separateEvenOdd(Stack& original, Stack& evenStack, Stack& oddStack) {
+    while (!original.isEmpty()) {
+        int value = original.pop();
         if (value % 2 == 0) {
             evenStack.push(value);
         } else {
@@ -18,32 +58,21 @@ void separateEvenOdd(const stack<int>& inputStack, stack<int>& evenStack, stack<
     }
 }
 
-void printStack(stack<int> s) {
-    while (!s.empty()) {
-        cout << s.top() << " ";
-        s.pop();
-    }
-    cout << endl;
-}
-
 int main() {
-    stack<int> inputStack;
-    vector<int> inputValues = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Stack original, evenStack, oddStack;
 
-    for (int value : inputValues) {
-        inputStack.push(value);
-    }
+    original.push(10);
+    original.push(15);
+    original.push(20);
+    original.push(25);
+    original.push(30);
+    separateEvenOdd(original, evenStack, oddStack);
 
-    stack<int> evenStack;
-    stack<int> oddStack;
+    cout << "Even stack: ";
+    evenStack.display();
 
-    separateEvenOdd(inputStack, evenStack, oddStack);
-
-    cout << "Even Stack: ";
-    printStack(evenStack);
-
-    cout << "Odd Stack: ";
-    printStack(oddStack);
+    cout << "Odd stack: ";
+    oddStack.display();
 
     return 0;
 }
