f1a1fe170eedb038be38a314848e28a6f36e20ea
Syed Huzaifa Ali <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-10-18 12:20:58 +0500

    Update task02.cpp

--- Labs/07/task02.cpp
+++ Labs/07/task02.cpp
@@ -1,1 +1,79 @@
+#include <iostream>
+using namespace std;
+
+class Runner {
+public:
+    string name;
+    int finishTime;
+};
+
+void merge(Runner arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    Runner* leftArr = new Runner[n1];
+    Runner* rightArr = new Runner[n2];
+
+    for (int i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        rightArr[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (leftArr[i].finishTime <= rightArr[j].finishTime) {
+            arr[k] = leftArr[i];
+            i++;
+        } else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+
+    delete[] leftArr;
+    delete[] rightArr;
+}
+
+void mergeSort(Runner arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    const int numRunners = 10;
+    Runner runners[numRunners];
+
+    for (int i = 0; i < numRunners; i++) {
+        cout << "Enter name of runner " << i + 1 << ": ";
+        cin >> runners[i].name;
+        cout << "Enter finish time (in seconds) for " << runners[i].name << ": ";
+        cin >> runners[i].finishTime;
+    }
+
+    mergeSort(runners, 0, numRunners - 1);
+
+    cout << "\nTop 5 Fastest Runners:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << i + 1 << ". " << runners[i].name << " - " << runners[i].finishTime << " seconds\n";
+    }
+}
 
