74b8b6efcc0da07d8051366370e6319b11cddf5c
Syed Huzaifa Ali <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-10-18 12:33:20 +0500

    Update task04.cpp

--- Labs/07/task04.cpp
+++ Labs/07/task04.cpp
@@ -1,1 +1,84 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 10;
+
+int getMax(int arr[], int n) {
+    int maxVal = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+    return maxVal;
+}
+
+void countingSort(int arr[], int n, int exp) {
+    int output[MAX];
+    int count[10];
+
+    for (int i = 0; i < 10; i++) {
+        count[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int digit = (arr[i] / exp) % 10;
+        count[digit]++;
+    }
+
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        int digit = (arr[i] / exp) % 10;
+        output[count[digit] - 1] = arr[i];
+        count[digit]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void radixSortAscending(int arr[], int n) {
+    int maxVal = getMax(arr, n);
+
+    for (int exp = 1; maxVal / exp > 0; exp *= 10) {
+        countingSort(arr, n, exp);
+    }
+}
+
+void reverseArray(int arr[], int n) {
+    for (int i = 0; i < n / 2; i++) {
+        swap(arr[i], arr[n - 1 - i]);
+    }
+}
+
+int main() {
+    int arr[MAX] = {170, 45, 75, 90, 802, 24, 2, 66, 128, 45};
+    int n = MAX;
+    
+    cout << "Unsorted Array:" << endl;
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    radixSortAscending(arr, n);
+
+    cout << "\nArray sorted in ascending order:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    reverseArray(arr, n);
+
+    cout << "\nArray sorted in descending order:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
